37a23b904ed3b48567380f725ff8bcabadd7785e
justinpsiegel <justinpsiegel@gmail.com>
2023-04-18 16:37:24 -0400

    Created individual product GET route

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import AuthForm from '../features/auth/AuthForm';
-import Home from '../features/home/Home';
-import { me } from './store';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import AuthForm from "../features/auth/AuthForm";
+import Home from "../features/home/Home";
+import { me } from "./store";
 
 /**
  * COMPONENT
@@ -14,7 +14,7 @@ const AppRoutes = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(me());
+    dispatch(me);
   }, []);
 
   return (
