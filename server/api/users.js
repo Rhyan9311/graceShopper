450eb30257e011e0d1dee8d92fb1eadc7eaebd82
justinpsiegel <justinpsiegel@gmail.com>
2023-04-19 17:44:40 -0400

    Started route for carts via users

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const {
-  models: { User },
+  models: { User, Cart, Product },
 } = require("../db");
 module.exports = router;
 
@@ -16,4 +16,20 @@ router.get("/", async (req, res, next) => {
   } catch (err) {
     next(err);
   }
+
+  router.get("/:id", async (req, res, next) => {
+    try {
+      const userCart = await Cart.findAll(
+        { where: { userId: req.params.id } },
+        {
+          include: {
+            model: Product, //This include doesn't seem to do anything yet
+          },
+        }
+      );
+      res.json(userCart);
+    } catch (error) {
+      next(error);
+    }
+  });
 });
